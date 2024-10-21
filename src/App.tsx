d65ab16ceb33bb516fae359ba91d2f8b7831e0b2
Blissito <fixtergeek@gmail.com>
2024-10-20 19:26:39 -0600

    draft versio

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Flipper } from "./components/Flipper";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Flipper>
+        <div className="bg-indigo-500">
+          <img
+            src="https://i.pinimg.com/originals/da/06/e1/da06e1be9c12e606745ef470748937da.png"
+            alt="perro"
+          />
+        </div>
+        <div className="bg-pink-500">
+          <img
+            src="https://png.pngtree.com/png-vector/20240606/ourmid/pngtree-black-logo-with-a-leaf-logo-vector-png-image_6991328.png"
+            alt="perros"
+          />
+        </div>
+        <div className="bg-slate-500">
+          <img
+            src="https://png.pngtree.com/png-vector/20240626/ourmid/pngtree-flying-eagle-black-logo-vector-png-image_12713602.png"
+            alt="aguila"
+          />
+        </div>
+      </Flipper>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
