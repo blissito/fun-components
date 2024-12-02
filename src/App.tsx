249e7eaeeb1886de895206466f3c6c4189eaa231
Blissito <fixtergeek@gmail.com>
2024-12-02 08:17:27 -0600

    flipWords started

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
 import "./App.css";
-import { LinkPreview } from "./components/LinkPreview";
+import { FlipWords } from "./components/FlipWords";
 
 function App() {
   return (
     <article className="flex items-center h-[80vh]">
-      <section className="text-md text-justify">
-        Esto es un texto de{" "}
-        <LinkPreview
-          className="text-indigo-500"
-          src="https://i.imgur.com/ArpsLvc.png"
-        >
-          prueba para demostrar
-        </LinkPreview>{" "}
-        el componente LinkPreview. <br />
-        Intentaremos no revelar nada que sea clasificado. No se mencionará el{" "}
-        <LinkPreview
-          className="text-orange-500"
-          src="https://i.imgur.com/vu1iTcr.png"
-        >
-          podcast de FixterGeek
-        </LinkPreview>{" "}
-        al que se le ha clasificado como incitador de la renuncia en favor de la
-        creatividad. 😱
-      </section>
+     <FlipWords>
     </article>
   );
 }
---
+++ src/components/FlipWords.tsx
@@ -0,0 +1,3 @@
+export const FlipWords = () => {
+  return <div>perro</div>;
+};
