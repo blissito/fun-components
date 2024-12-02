27d375293d17d618864f7011b4278934a7f6009a
Blissito <fixtergeek@gmail.com>
2024-12-02 10:12:09 -0600

    FlipWords ready

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,20 @@ import { FlipWords } from "./components/FlipWords";
 
 function App() {
   return (
-    <article className="max-w-xl mx-auto">
-      <FlipWords>
-        <h1 className="text-4xl text-left mb-3">Animaciones web con Motion</h1>
+    <article className="max-w-xl mx-auto py-20 px-4 bg-gradient-to-tr from-blue-500/50 to-indigo-500/50 rounded-lg">
+      <FlipWords delay={0.03}>
+        <h1 className="text-4xl text-left mb-3 font-bold tracking-tight">
+          Animaciones web con Motion
+        </h1>
       </FlipWords>
       <p className="text-left max-w-xl text-xl">
-        Las animaciones web lo tienen todo.
-        <br />
-        Pueden embellecer, destacar o enamorar a cualquiera que las experimenta
-        (los usuarios). Pero, también tienen la capacidad de mejorar tus
-        habilidades lógicas y de pensamiento matemático. Ocacionando así, uno de
-        los mejores fenómenos del espíritu. El aprendizaje que lleva a la mejora
-        sistemática a partir de la creación de múltiples componentes animados;
-        cada vez más complejos. 🎩🤩🪄📚🤯
+        Las animaciones web lo tienen todo. Pueden embellecer, destacar o
+        enamorar a cualquiera que las experimenta (los usuarios). Pero, también
+        tienen la capacidad de mejorar tus habilidades lógicas y de pensamiento
+        matemático. Ocacionando así, uno de los mejores fenómenos del espíritu.
+        El aprendizaje que lleva a la mejora sistemática a partir de la creación
+        de múltiples componentes animados; cada vez más complejos. 🎩🤩🪄📚🤯
       </p>
-      <button className="py-2 px-6 bg-blue-800 text-white font-bold transition-all hover:bg-blue-700 active:bg-blue-600 my-4 shadow-lg active:shadow">
-        Animar de nuevo
-      </button>
     </article>
   );
 }
--- src/components/FlipWords.tsx
+++ src/components/FlipWords.tsx
@@ -1,5 +1,62 @@
-import { ReactNode } from "react";
+import { Children, ReactNode, useEffect, useState } from "react";
+import { motion } from "motion/react";
+import { cn } from "../utils/cn";
+import { nanoid } from "nanoid";
 
-export const FlipWords = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+export const FlipWords = ({
+  delay = 0.03,
+  children,
+}: {
+  delay?: number;
+  children: ReactNode;
+}) => {
+  const [letters, setLetters] = useState<ReactNode[]>([]);
+  const wordNode = Children.toArray(children)[0] as ReactNode;
+
+  const replaceNodes = () => {
+    let letrs = wordNode?.props?.children;
+    letrs = letrs.split("");
+    letrs = letrs.map((letter: string, i: number) => {
+      return letter === " " ? (
+        <span key={i}>&nbsp;</span>
+      ) : (
+        <motion.span
+          children={letter}
+          initial={{
+            scale: 4,
+            y: 20,
+            opacity: 0,
+            rotateY: -180,
+            rotateZ: 180,
+            filter: "blur(9px)",
+          }}
+          animate={{
+            scale: 1,
+            y: 0,
+            opacity: 1,
+            rotateY: 0,
+            rotateZ: 0,
+            filter: "blur(0px)",
+          }}
+          transition={{ delay: delay * i }}
+          key={nanoid()} // truco de mágia 🪄
+        />
+      );
+    });
+    setLetters(letrs);
+  };
+
+  useEffect(() => {
+    replaceNodes();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <button
+      onClick={replaceNodes}
+      className={cn("text-left flex", wordNode?.props?.className)}
+    >
+      {letters}
+    </button>
+  );
 };
